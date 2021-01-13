9fa9340dcd72fc86081f5d37b414b569ae7c3f21
AbhishekAwate <72369394+AbhishekAwate@users.noreply.github.com>
2021-01-13 06:32:37 -0800

    prime or not.c
    This code will tell us that the entered number is prime or not.

---
+++ prime or not.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int prime_or_not(int, int);
+int main()
+{
+    int num, prime;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    prime = prime_or_not(num, num/2);
+    if(prime % 2 == 0)
+    {
+		printf("\n%d is not a prime number\n", num);
+    }
+    else
+    {
+    
+	  printf("\n%d is a prime number\n", num);
+    }
+    return 0;
+}
+int prime_or_not(int n, int i)
+{
+    if(i == 1)
+       return 1;  
+    else
+    {
+       if(n%i == 0)
+           return 0;
+       else
+           prime_or_not(n, i-1);    
+    }
+}
